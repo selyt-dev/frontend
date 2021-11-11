352473f5ce2d46d43e9f6aa1982503b66aa904b3
David Paiva <pillgp@icloud.com>
2021-11-11 14:27:57 +0000

    Added React Native Paper for UI and reformulated how pages render.

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import * as Animatable from 'react-native-animatable'
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
-import { Sae } from 'react-native-textinput-effects'
-import React, { useRef } from 'react'
-import { StyleSheet, Image, Text, View, Button, TextInput } from 'react-native'
+import Main from './views/Main.js'
+
+import { Provider as PaperProvider } from 'react-native-paper'
+import { AppRegistry } from 'react-native'
+import React from 'react'
+import { name as appName } from './app.json';
 import { useFonts } from 'expo-font'
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App () {
-  const state = useRef({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    birthDate: '',
-    nif: ''
-  })
-
+export default function App() {
   const [loaded] = useFonts({
     CoolveticaRegular: require('./assets/fonts/coolvetica-rg.ttf'),
     MontserratRegular: require('./assets/fonts/Montserrat-Regular.ttf')
@@ -25,65 +18,12 @@ export default function App () {
   }
 
   return (
-    <Animatable.View style={styles.container}>
-      <Animatable.Image
-        animation='fadeInUp'
-        style={styles.image}
-        source={require('./assets/selyt-transparent.png')}
-      />
-      <Animatable.Text animation='fadeInUp' style={styles.logoText}>
-        Selyt
-      </Animatable.Text>
-
-      <Text>&nbsp;</Text>
-
-      <Animatable.View animation='fadeInUp' style={styles.fixToText}>
-        <Button
-          style={styles.button}
-          title='Login'
-          color='#333333'
-          onPress={() => Alert.alert('Left button pressed')}
-        />
-        <Text>&nbsp;</Text>
-        <Text>&nbsp;</Text>
-        <Text>&nbsp;</Text>
-        <Button
-          style={styles.button}
-          title='Registo'
-          color='#333333'
-          onPress={() => Alert.alert('Right button pressed')}
-        />
-      </Animatable.View>
-    </Animatable.View>
+    <NavigationContainer>
+      <PaperProvider>
+        <Main />
+      </PaperProvider>
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    color: '#fff',
-    backgroundColor: '#222',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  container2: {
-    width: '100%'
-  },
-  image: {
-    width: 150,
-    height: 150
-  },
-  button: {
-    width: '100%',
-    backgroundColor: '#fff',
-  },
-  logoText: {
-    color: '#fff',
-    fontFamily: 'CoolveticaRegular',
-    fontSize: 35
-  }
-})
+AppRegistry.registerComponent(appName, () => App);
---
+++ views/Main.js
@@ -0,0 +1,69 @@
+import * as Animatable from 'react-native-animatable'
+import { Button } from 'react-native-paper'
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+export default function Main() {
+  return (
+    <Animatable.View style={styles.container}>
+      <Animatable.Image
+        animation='fadeInUp'
+        style={styles.image}
+        source={require('../assets/selyt-transparent.png')}
+      />
+      <Animatable.Text animation='fadeInUp' style={styles.logoText}>
+        Selyt
+      </Animatable.Text>
+
+      <Text>&nbsp;</Text>
+
+      <Animatable.View animation='fadeInUp' style={styles.fixToText}>
+        <Button
+          mode='contained'
+          color='#333333'
+          dark='true'
+          onPress={() => console.log('Left button pressed')}
+        >
+          Login
+        </Button>
+        <Text>&nbsp;</Text>
+        <Text>&nbsp;</Text>
+        <Text>&nbsp;</Text>
+        <Button
+          mode='contained'
+          color='#333333'
+          dark='true'
+          onPress={() => console.log('Right button pressed')}
+        >
+          Registo
+        </Button>
+      </Animatable.View>
+    </Animatable.View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: '#fff',
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  container2: {
+    width: '100%'
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  logoText: {
+    color: '#fff',
+    fontFamily: 'CoolveticaRegular',
+    fontSize: 35
+  }
+})
