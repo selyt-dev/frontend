9f52854081ad59ada20558bb7171f6dc324c501f
David Paiva <pillgp@icloud.com>
2022-01-09 12:54:39 +0000

    Users can now update their password.

---
+++ views/account/Account.js
@@ -0,0 +1,251 @@
+import {
+  Avatar,
+  Card,
+  List,
+  Dialog,
+  Paragraph,
+  Button,
+} from "react-native-paper";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  StatusBar,
+  ScrollView,
+  View,
+  SafeAreaView,
+  Pressable,
+  Image,
+} from "react-native";
+
+import Footer from "../components/Footer";
+import { NativeModules } from "react-native";
+
+import { getUserData } from "../../utils/react/DataStore";
+
+import moment from "moment/min/moment-with-locales";
+
+import * as ImagePicker from "expo-image-picker";
+import * as ImageManipulator from "expo-image-manipulator";
+
+import API from "../../utils/API";
+
+import * as SecureStore from "expo-secure-store";
+
+import { clearUserData } from "../../utils/react/DataStore";
+
+module.exports = class Account extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: null, logoutVisible: false };
+
+    this.changeAvatar = this.changeAvatar.bind(this);
+  }
+
+  async componentDidMount() {
+    moment.locale(NativeModules.I18nManager.localeIdentifier);
+    getUserData().then((user) => {
+      this.setState({ user });
+    });
+  }
+
+  async changeAvatar() {
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      } else {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+
+        if (!result.cancelled) {
+          const image = await ImageManipulator.manipulateAsync(
+            result.uri,
+            [{ resize: { width: 128, height: 128 } }],
+            {
+              base64: true,
+              compress: 0.7,
+              format: ImageManipulator.SaveFormat.JPEG,
+            }
+          );
+
+          const data = await API.updateAvatar(
+            await SecureStore.getItemAsync("authorization"),
+            `data:image/jpeg;base64,${image.base64}`
+          );
+
+          const response = await data.json();
+
+          if (!response.ok) {
+            alert("Erro ao atualizar avatar!");
+          }
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.insideContainer}>
+          <Card style={styles.card}>
+            <Card.Content style={styles.adCard}>
+              <View style={styles.avatarContainer}>
+                <Pressable onPress={this.changeAvatar}>
+                  {this.state.user?.hasAvatar ? (
+                    <Avatar.Image
+                      style={{ width: 100, height: 100 }}
+                      size={100}
+                      source={{
+                        uri: `https://s3.eu-west-3.amazonaws.com/cdn.selyt.pt/users/${this.state.user?.id}.jpg`,
+                      }}
+                    />
+                  ) : (
+                    <Avatar.Icon size={100} icon="account" />
+                  )}
+                </Pressable>
+                <Text style={styles.name}>{this.state.user?.name}</Text>
+                <Text style={styles.sub}>
+                  Membro desde{" "}
+                  {moment(this.state.user?.createdAt)
+                    .format("MMMM YYYY")
+                    .toString()}
+                </Text>
+              </View>
+            </Card.Content>
+          </Card>
+
+          <Text>&nbsp;</Text>
+
+          <Card style={styles.card}>
+            <Card.Content style={styles.card}>
+              <List.Section>
+                <List.Subheader style={styles.subtitle}>
+                  A sua conta
+                </List.Subheader>
+                <List.Item
+                  title="Definições"
+                  left={() => <List.Icon icon="cog" />}
+                  onPress={() =>
+                    this.props.navigation.navigate("AccountSettings")
+                  }
+                />
+                <List.Item
+                  title="Saldo"
+                  left={() => <List.Icon icon="currency-usd" />}
+                />
+                <List.Item
+                  title="Os seus anúncios"
+                  left={() => <List.Icon icon="post" />}
+                />
+                <List.Item
+                  title="Suporte"
+                  left={() => <List.Icon icon="help-circle" />}
+                />
+                <List.Item
+                  title="Sair"
+                  titleStyle={styles.logout}
+                  left={() => <List.Icon icon="exit-to-app" color="#ff3b3b" />}
+                  onPress={() => this.setState({ logoutVisible: true })}
+                />
+              </List.Section>
+            </Card.Content>
+          </Card>
+        </ScrollView>
+        <Footer />
+        <Dialog
+          visible={this.state.logoutVisible}
+          onDismiss={() => this.setState({ logoutVisible: false })}
+        >
+          <Dialog.Title>Alerta</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>Tem a certeza que quer sair?</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => this.setState({ logoutVisible: false })}>
+              Não
+            </Button>
+            <Button
+              onPress={async () => {
+                await clearUserData();
+                this.props.navigation.navigate("Main");
+              }}
+            >
+              Sim
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </SafeAreaView>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: "#fff",
+    backgroundColor: "#222",
+    paddingTop: StatusBar.currentHeight,
+  },
+  avatarContainer: {
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 24,
+    marginTop: 10,
+    color: "#fff",
+  },
+  sub: {
+    fontSize: 14,
+    marginTop: 8,
+    color: "#fff",
+  },
+  subtitle: {
+    fontSize: 20,
+    marginTop: 8,
+    color: "#fff",
+  },
+  insideContainer: {
+    flex: 1,
+    color: "#fff",
+    backgroundColor: "#222",
+    marginBottom: 56,
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  container2: {
+    width: "100%",
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+  logoText: {
+    color: "#fff",
+    fontFamily: "CoolveticaRegular",
+    fontSize: 35,
+  },
+  textInput: {
+    marginBottom: 7,
+  },
+  adCard: {
+    flex: 1,
+    alignContent: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  card: {
+    flex: 1,
+    //    flexDirection: "row",
+  },
+  logout: {
+    color: "#ff3b3b",
+  },
+});
---
+++ views/account/AccountSettings.js
@@ -0,0 +1,323 @@
+import { Card, TextInput, Button } from "react-native-paper";
+import React from "react";
+import { StyleSheet, Text, StatusBar, SafeAreaView } from "react-native";
+
+import { NativeModules } from "react-native";
+import * as SecureStore from "expo-secure-store";
+
+import { getUserData, clearUserData } from "../../utils/react/DataStore";
+
+import moment from "moment/min/moment-with-locales";
+
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import API from "../../utils/API";
+
+module.exports = class AccountSettings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _user: null, // Original user data
+      user: null,
+      password: null,
+      newPassword: null,
+      newPasswordConfirmation: null,
+      hidePassword: true,
+      hideNewPassword: true,
+      hideNewPasswordConfirmation: true,
+      hidePasswordIcon: "eye",
+      hideNewPasswordIcon: "eye",
+      hideNewPasswordConfirmationIcon: "eye",
+    };
+
+    this._save = this._save.bind(this);
+    this.changeIcon = this.changeIcon.bind(this);
+    this.changeNewIcon = this.changeNewIcon.bind(this);
+    this.changeNewConfirmationIcon = this.changeNewConfirmationIcon.bind(this);
+  }
+
+  async componentDidMount() {
+    moment.locale(NativeModules.I18nManager.localeIdentifier);
+    getUserData().then((user) => {
+      this.setState({ user, _user: user });
+    });
+  }
+
+  async _save() {
+    // TODO: validate data & show proper alerts for stuff
+    // TODO: check if password is correct
+    try {
+      console.log(this.state.user === this.state._user);
+
+      console.log(
+        this.state.password !== null &&
+          this.state.newPassword !== null &&
+          this.state.newPasswordConfirmation !== null
+      );
+
+      const authorization = await SecureStore.getItemAsync("authorization");
+
+      if (
+        this.state.password !== null &&
+        this.state.newPassword !== null &&
+        this.state.newPasswordConfirmation !== null
+      ) {
+        // Change password
+        await API.changePassword(authorization, {
+          password: this.state.password,
+          newPassword: this.state.newPassword,
+          newPasswordConfirmation: this.state.newPasswordConfirmation,
+        })
+          .then((res) => res.json())
+          .then(async (res) => {
+            console.log(res);
+            if (res.ok) {
+              alert("Senha alterada com sucesso!");
+              await clearUserData();
+              this.props.navigation.navigate("Main");
+            } else {
+              alert("Erro ao alterar senha!");
+            }
+          })
+          .catch((err) => {
+            alert("Erro ao alterar senha!");
+          });
+      }
+
+      if (this.state.user === this.state._user) {
+        return;
+      }
+
+      await API.updateSelf(authorization, {
+        name: this.state.user.name,
+        email: this.state.user.email,
+        nif: this.state.user.nif,
+        phone: this.state.user.phone,
+        iban: this.state.user.iban,
+      })
+        .then((res) => res.json())
+        .then(async (res) => {
+          if (res.ok) {
+            await clearUserData();
+            this.props.navigation.navigate("Main");
+          } else {
+            alert("Erro ao atualizar dados");
+          }
+        });
+    } catch (error) {
+      alert("Não foi possível atualizar os dados");
+      console.log(error);
+    }
+  }
+
+  async changeIcon() {
+    this.setState({
+      hidePassword: !this.state.hidePassword,
+      hidePasswordIcon: this.state.hidePassword ? "eye-off" : "eye",
+    });
+  }
+
+  async changeNewIcon() {
+    this.setState({
+      hideNewPassword: !this.state.hideNewPassword,
+      hideNewPasswordIcon: this.state.hideNewPassword ? "eye-off" : "eye",
+    });
+  }
+
+  async changeNewConfirmationIcon() {
+    this.setState({
+      hideNewPasswordConfirmation: !this.state.hideNewPasswordConfirmation,
+      hideNewPasswordConfirmationIcon: this.state.hideNewPasswordConfirmation
+        ? "eye-off"
+        : "eye",
+    });
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <KeyboardAwareScrollView
+          style={styles.insideContainer}
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          scrollEnabled={true}
+        >
+          <Card style={styles.card}>
+            <Card.Content style={styles.card}>
+              <Text style={styles.logoText}>Definições</Text>
+              <Text>&nbsp;</Text>
+              <TextInput
+                label="Nome"
+                value={this.state.user?.name}
+                style={styles.textInput}
+                onChangeText={(text) =>
+                  this.setState({ user: { ...this.state.user, name: text } })
+                }
+                left={<TextInput.Icon name="account" />}
+                placeholder="João Santos"
+              />
+              <TextInput
+                label="Email"
+                value={this.state.user?.email}
+                style={styles.textInput}
+                onChangeText={(text) =>
+                  this.setState({ user: { ...this.state.user, email: text } })
+                }
+                keyboardType="email-address"
+                left={<TextInput.Icon name="email" />}
+                placeholder="joão.santos@selyt.pt"
+              />
+              <TextInput
+                label="NIF"
+                value={this.state.user?.nif.toString()}
+                style={styles.textInput}
+                onChangeText={(text) =>
+                  this.setState({ user: { ...this.state.user, nif: text } })
+                }
+                keyboardType="phone-pad"
+                left={<TextInput.Icon name="card-account-details" />}
+                placeholder="123 456 789"
+              />
+              <TextInput
+                label="Número de Telemóvel"
+                value={this.state.user?.phone.toString()}
+                style={styles.textInput}
+                onChangeText={(text) =>
+                  this.setState({ user: { ...this.state.user, phone: text } })
+                }
+                keyboardType="phone-pad"
+                left={<TextInput.Icon name="card-account-phone" />}
+                placeholder="912 345 678"
+              />
+              <TextInput
+                label="IBAN"
+                value={this.state.user?.iban}
+                style={styles.textInput}
+                onChangeText={(text) =>
+                  this.setState({ user: { ...this.state.user, iban: text } })
+                }
+                left={<TextInput.Icon name="bank" />}
+                placeholder="PT50 0011 2213 3456 7890 0002 3"
+              />
+              <TextInput
+                label="Palavra-passe atual"
+                value={this.state.password}
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({ password: text })}
+                secureTextEntry={this.state.hidePassword}
+                left={<TextInput.Icon name="form-textbox-password" />}
+                right={
+                  <TextInput.Icon
+                    name={this.state.hidePasswordIcon}
+                    onPress={() => this.changeIcon()}
+                  />
+                }
+              />
+              <TextInput
+                label="Nova palavra-passe"
+                value={this.state.newPassword}
+                style={styles.textInput}
+                onChangeText={(text) => this.setState({ newPassword: text })}
+                secureTextEntry={this.state.hideNewPassword}
+                left={<TextInput.Icon name="form-textbox-password" />}
+                right={
+                  <TextInput.Icon
+                    name={this.state.hideNewPasswordIcon}
+                    onPress={() => this.changeNewIcon()}
+                  />
+                }
+              />
+              <TextInput
+                label="Confirmar nova palavra-passe"
+                value={this.state.newPasswordConfirmation}
+                style={styles.textInput}
+                onChangeText={(text) =>
+                  this.setState({ newPasswordConfirmation: text })
+                }
+                secureTextEntry={this.state.hideNewPasswordConfirmation}
+                left={<TextInput.Icon name="form-textbox-password" />}
+                right={
+                  <TextInput.Icon
+                    name={this.state.hideNewPasswordConfirmationIcon}
+                    onPress={() => this.changeNewConfirmationIcon()}
+                  />
+                }
+              />
+              <Text>&nbsp;</Text>
+              <Button
+                mode="contained"
+                color="#333333"
+                dark="true"
+                onPress={() => this._save()}
+              >
+                Salvar
+              </Button>
+            </Card.Content>
+          </Card>
+        </KeyboardAwareScrollView>
+      </SafeAreaView>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: "#fff",
+    backgroundColor: "#222",
+    paddingTop: StatusBar.currentHeight,
+  },
+  avatarContainer: {
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 24,
+    marginTop: 10,
+    color: "#fff",
+  },
+  sub: {
+    fontSize: 14,
+    marginTop: 8,
+    color: "#fff",
+  },
+  subtitle: {
+    fontSize: 20,
+    marginTop: 8,
+    color: "#fff",
+  },
+  insideContainer: {
+    flex: 1,
+    color: "#fff",
+    backgroundColor: "#222",
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  container2: {
+    width: "100%",
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+  logoText: {
+    color: "#fff",
+    fontFamily: "CoolveticaRegular",
+    fontSize: 35,
+  },
+  textInput: {
+    marginBottom: 7,
+  },
+  adCard: {
+    flex: 1,
+    alignContent: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  card: {
+    flex: 1,
+    //    flexDirection: "row",
+  },
+  logout: {
+    color: "#ff3b3b",
+  },
+});
