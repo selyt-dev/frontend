70d66acc5350bbb02b05ff539bd2cfaeea67f923
David Paiva <pillgp@icloud.com>
2022-01-16 12:43:11 +0000

    Undoing something I tried to make KEKW

--- utils/react/ThemeModule.js
+++ utils/react/ThemeModule.js
@@ -1,17 +1,8 @@
 import { Appearance } from "react-native";
 import { DefaultTheme, DarkTheme } from "./themes/index";
 
-import * as SecureStore from "expo-secure-store";
-
 module.exports = class ThemeModule {
-  static THEME =
-    SecureStore.getItemAsync("theme") === "auto"
-      ? Appearance.getColorScheme() === "dark"
-        ? DarkTheme
-        : DefaultTheme
-      : SecureStore.getItemAsync("theme") === "dark"
-      ? DarkTheme
-      : DefaultTheme;
+  static THEME = Appearance.getColorScheme();
   static IS_DARK_THEME = ThemeModule.THEME === "dark";
   static THEME_OBJECT = ThemeModule.IS_DARK_THEME ? DarkTheme : DefaultTheme;
 };
