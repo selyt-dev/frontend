567bced7382be37cfab0326461cd05517957cf96
David Paiva <pillgp@icloud.com>
2022-03-02 10:01:59 +0000

    🍱 Changed the default image to our own.

--- views/components/AdCard.js
+++ views/components/AdCard.js
@@ -39,7 +39,7 @@ module.exports = class AdCard extends React.Component {
           <Image
             style={styles.image}
             source={{
-              uri: "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg",
+              uri: "https://cdn.selyt.pt/ads/default.jpg",
             }}
           />
         )}
